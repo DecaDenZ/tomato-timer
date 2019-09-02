bac216e301046c72e272bcc9c740b69f2692c0d0
Denis Potapov <potapovdenis09@gmail.com>
2019-09-02 21:25:51 +0300

    работает функция таймера, добавить остановку по времени и запускт нажатием старт

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 // разделить работу приложения на 3 фазы
 // фааза выбора задачи, рабочая фаза = 25 минут работы четчика,
-//фаза отдыха = 5 - 20 минут работы счетсчика
+//фаза отдыха = 5 - 20 минут работы счетчика
 const TIME_TO_WORK = 1500000; //25 минут в мс
 const TIME_TO_LITTLE_REST = 300000; // 5 минут в милисекундах
 const TIME_TO_BIG_REST = 900000; // 15 минут в мс
 var activeTask = 'Выберите задачу';
-var time = TIME_TO_WORK;
+var time = TIME_TO_WORK; // глобальная переменная, нужен глобальный доступ чтобы была возможность сброса таймера
 
 function addTask(){
    var newTask = prompt('Введите задачу');
@@ -13,25 +13,24 @@ function addTask(){
    $(".task-item:last").after(taskItem);
 }
 
-function timer(time){
+function timer(){
+   time = time - 1000;
    var countdown = new Date(time);
    $(".timer").empty();
-   $(".timer").append("<div>" + countdown + "</div>");
-   countdown = countdown - 1000;
+   $(".timer").append("<div>" + countdown.getMinutes() + " : " + countdown.getSeconds() + "</div>");
+}
+
+function startTimer(){
+
 }
 
 
 $(document).ready(function(){
    'use strict';
    $(".add-task").click(addTask);
-   console.log(time);
-   let timerId = setInterval ((time) => {   // вынести в отдельную функцию, когда начнет работать
-      console.log(typeof(time));
-      var countdown = new Date(time);
-      $(".timer").empty();
-      $(".timer").append("<div>" + countdown + "</div>");
-      time = time - 1000;
-   }, 1000 );
-   setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000);
+   let timerId = setInterval (timer(), 1000);
+   setTimeout(() => { clearInterval(timerId); alert('stop'); }, 15000);
+
+   $("#button-start").click(startTimer);
 
 })
