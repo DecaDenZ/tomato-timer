960cd6f95bbee3f39623190c6db7ebbd7fd016e2
DecaDenZ <potapovdenis09@gmail.com>
2019-09-03 17:19:29 +0300

    все ровно, ничего никуда не уежает, только кнопка добавления задачи не совпадает размером со списком задачь и не поцентру

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,30 @@
 // разделить работу приложения на 3 фазы
 // фааза выбора задачи, рабочая фаза = 25 минут работы четчика,
 //фаза отдыха = 5 - 20 минут работы счетчика
-const TIME_TO_WORK = 1500000; //25 минут в мс
+const TIME_TO_WORK = 1499000; //25 минут в мс
 const TIME_TO_LITTLE_REST = 300000; // 5 минут в милисекундах
 const TIME_TO_BIG_REST = 900000; // 15 минут в мс
+const PHASE_REST = 'rest';
+const PHASE_WORK = 'work';
+const PHASE_STOP = 'stop';
+
+var activePhase = PHASE_STOP;
 var activeTask = 'Выберите задачу';
-var time = TIME_TO_WORK-1000; // глобальная переменная, нужен глобальный доступ
+var time = TIME_TO_WORK; // глобальная переменная, нужен глобальный доступ
 //чтобы была возможность сброса таймера из других функций
+
+function chooseCurrentTask(e){
+   $(".active-task h1").text(e.currentTarget.innerHTML);
+   stopTimer();
+}
 
 function addTask(){
    var newTask = prompt('Введите задачу');
-   var taskItem = $(".task-item:last").clone().text(newTask);
-   $(".task-item:last").after(taskItem);
+   // var taskItem = $(".task-item:last").clone().text(newTask);
+
+   $(".task-list").append("<li class='task-item'></li>");
+   $(".task-item:last").text(newTask);
+   $(".task-item").on('click', (e) => chooseCurrentTask(e));
 }
 
 function timer(){
@@ -26,18 +39,22 @@ function timer(){
 }
 
 function startTimer(){
+   activePhase = PHASE_WORK;
   let timerId = setInterval (timer, 1000);
   setTimeout(() => { clearInterval(timerId); alert('stop'); }, 15000);
 }
 
-function chooseCurrentTask(e){
-  console.log(e.currentTarget.innerHTML);
-  $(".active-task h1").empty();
+function stopTimer(){
+   time = TIME_TO_WORK;
+   activePhase = PHASE_STOP;
+   console.log(time);
 }
+
 
 $(document).ready(function(){
    'use strict';
    $(".add-task").click(addTask);
    $("#button-start").click(startTimer);
+   $("#button-stop").click(stopTimer);
    $(".task-item").on('click', (e) => chooseCurrentTask(e));
 })
