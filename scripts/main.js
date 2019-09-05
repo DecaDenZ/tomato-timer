5d0fd1a9832e401e961a64a7b8e33ab11eae91b5
DecaDenZ <potapovdenis09@gmail.com>
2019-09-05 11:49:07 +0300

    скорректирована работа таймера

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,12 @@
 // разделить работу приложения на 3 фазы
 // фааза выбора задачи, рабочая фаза = 25 минут работы четчика,
 //фаза отдыха = 5 - 20 минут работы счетчика
-const TIME_TO_WORK = 14000; //25 минут в мс
-const TIME_TO_LITTLE_REST = 30000; // 5 минут в милисекундах
-const TIME_TO_BIG_REST = 90000; // 15 минут в мс
+// const TIME_TO_WORK = 1499000; //25 минут в мс
+const TIME_TO_WORK = 14000;
+// const TIME_TO_LITTLE_REST = 300000; // 5 минут в милисекундах
+const TIME_TO_LITTLE_REST = 15000;
+// const TIME_TO_BIG_REST = 900000; // 15 минут в мс
+const TIME_TO_BIG_REST = 15000;
 const PHASE_REST = 'rest';
 const PHASE_WORK = 'work';
 const PHASE_STOP = 'stop';
@@ -34,7 +37,7 @@ function startTimer() {
 }
 
 function timer() {
-  console.log(counter);
+console.log(counter);
   var countdown = new Date(time);
   $(".timer").empty();
   $(".timer").append("<div>" + countdown.getMinutes()
@@ -42,20 +45,20 @@ function timer() {
 
   if (time === 0) {
     if (activePhase === PHASE_WORK) {
-      alert('Отдохните немного');
-      activePhase = PHASE_REST;
-      time = TIME_TO_LITTLE_REST;
+      if (counter < 4){
+         alert('Отдохните немного');
+         activePhase = PHASE_REST;
+         time = TIME_TO_LITTLE_REST;
+         counter++;
+      } else {
+         alert('Одохните побольше');
+         time = TIME_TO_BIG_REST;
+         counter = 1;
+      }
     } else {
-      if (counter < 4) {
         alert('Вернитесь к задаче');
         time = TIME_TO_WORK;
         activePhase = PHASE_WORK;
-        counter++;
-      } else {
-        alert('Одохните побольше');
-        time = TIME_TO_BIG_REST;
-        counter = 1;
-      }
       }
     }
   time -= 1000;
