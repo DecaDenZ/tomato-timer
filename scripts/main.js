b7b7f6bc33a8d869e15c5545649d5263bee515df
DecaDenZ <potapovdenis09@gmail.com>
2019-09-10 13:52:14 +0300

    к предыдущему

--- scripts/main.js
+++ scripts/main.js
@@ -21,92 +21,98 @@ var timerId;
 var counter = 1; // отсчет количества выполненных помидоров
 
 function chooseActiveTask(e) {
-  $(".active-task-item").removeClass("active-task-item").addClass("task-item");
-  var newActiveTask = e.currentTarget.innerHTML;
-  console.log(newActiveTask);
-  e.currentTarget.className = "active-task-item";
-  if (newActiveTask !== activeTask) {
-    $(".active-task h1").text(newActiveTask);
-    activeTask = newActiveTask;
-    stopTimer();
-  }
+   $(".active-task-item").attr("class", "task-item");
+   var newActiveTask = $(e.target).closest(".task-item");
+   newActiveTask.attr("class", "active-task-item");
+   var newActiveTaskName = newActiveTask.find(".task-name").text();
+   if (newActiveTaskName !== activeTask) {
+      $(".active-task h1").text(newActiveTaskName);
+      activeTask = newActiveTaskName;
+      stopTimer();
+   }
 }
 
 function addTask() {
-  var newTask = prompt('Введите задачу');
-  $(".task-item:last").clone(true).appendTo(".task-list");
-  $(".task-item:last").html(newTask + ` <span class="badge badge-primary badge-pill">0</span><div class="close-task">Х</div>`);
+   var newTask = prompt('Введите задачу');
+   $(".task-item:last").clone(true).appendTo(".task-list");
+   $(".task-item:last .task-name").text(newTask);
 }
 
 
 function deleteTask(e) {
-  e.currentTarget.remove;
+   if ($(".task-list li").length === 1) {
+      alert("в списке должна оставаться хотябы одна задача");
+      return;
+   }
+   var task = $(e.target);
+   task.parent().remove();
 }
 
 function endTask() {
-  activeTask = DEFAULT_TASK;
-  $(".active-task h1").text(activeTask);
-  $(".active-task-item").remove();
+   activeTask = DEFAULT_TASK;
+   $(".active-task h1").text(activeTask);
+   $(".active-task-item").remove();
 }
 
 function startTimer() {
-  if (activeTask === DEFAULT_TASK){
-    alert('Сначала выберите задачу, которую будете выполнять');
-    return
-  }
-  else {
-    activePhase = PHASE_WORK;
-    counter = 1;
-    timerId = setInterval(timer, 1000);
-  }
+   if (activeTask === DEFAULT_TASK) {
+      alert('Сначала выберите задачу, которую будете выполнять');
+      return
+   } else {
+      activePhase = PHASE_WORK;
+      counter = 1;
+      timerId = setInterval(timer, 1000);
+   }
 }
 
 function timer() {
-  var countdown = new Date(time);
-  $(".timer").empty();
-  $(".timer").append("<div>" + countdown.getMinutes() +
-    " : " + countdown.getSeconds() + "</div>");
+   var countdown = new Date(time);
+   $(".timer").empty();
+   $(".timer")
+      .append("<div>" + countdown.getMinutes() +
+         " : " + countdown.getSeconds() + "</div>");
 
-  if (time === 0) {
-    if (activePhase === PHASE_WORK) {
-      addTomato();
-      if (counter < 4) {
-        alert('Отдохните немного');
-        activePhase = PHASE_REST;
-        time = TIME_TO_LITTLE_REST;
-        counter++;
+   if (time === 0) {
+      if (activePhase === PHASE_WORK) {
+         addTomato();
+         if (counter < 4) {
+            alert('Отдохните немного');
+            activePhase = PHASE_REST;
+            time = TIME_TO_LITTLE_REST;
+            counter++;
+         } else {
+            alert('Одохните побольше');
+            time = TIME_TO_BIG_REST;
+            counter = 1;
+         }
       } else {
-        alert('Одохните побольше');
-        time = TIME_TO_BIG_REST;
-        counter = 1;
+         alert('Вернитесь к задаче');
+         time = TIME_TO_WORK;
+         activePhase = PHASE_WORK;
       }
-    } else {
-      alert('Вернитесь к задаче');
-      time = TIME_TO_WORK;
-      activePhase = PHASE_WORK;
-    }
-  } else {
-    time -= 1000;
-  }
+   } else {
+      time -= 1000;
+   }
 
 }
 
 function stopTimer() {
-  time = TIME_TO_WORK;
-  activePhase = PHASE_STOP;
-  clearInterval(timerId);
+   time = TIME_TO_WORK;
+   activePhase = PHASE_STOP;
+   clearInterval(timerId);
 }
 
 function addTomato() {
-  var tomatoes = $(".active-task-item span").text();
-  $(".active-task-item span").text(++tomatoes);
+   var tomatoes = $(".active-task-item .badge").text();
+   console.log(tomatoes);
+   $(".active-task-item .badge").text(++tomatoes);
 }
 
 $(document).ready(function() {
-  $(".add-task").click(addTask);
-  $("#button-start").click(startTimer);
-  $("#button-stop").click(stopTimer);
-  $("#button-end").click(endTask);
-  $(".task-item").on('click', (e) => chooseActiveTask(e));
-  $(".close-task").on('click', (e) => deleteTask(e));
+   $(".add-task").click(addTask);
+   $("#button-start").click(startTimer);
+   $("#button-stop").click(stopTimer);
+   $("#button-end").click(endTask);
+   $(".task-item").click((e) => chooseActiveTask(e));
+   $(".close-task").click((e) => deleteTask(e));
 })
